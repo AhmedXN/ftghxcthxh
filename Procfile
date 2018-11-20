82915f07ecf036c7329f20d32e0d0b9124e58095
AhmedXN <44243584+AhmedXN@users.noreply.github.com>
2018-11-20 14:46:10 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node bc.js
---
+++ bc.js
@@ -0,0 +1,41 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "c";
+var adminprefix = 'c'
+
+const developers = ["429217182140858380"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+client.login("mfa.wyze-OoQuPcf3OuVkqm9kcuV26yj2nEc8BXnaDXrpYUdyWLy9PCWOL9TgLtZX0PdhuWhkgRPfXb8mumkD_Ww");// لا تغير فيها شيء
